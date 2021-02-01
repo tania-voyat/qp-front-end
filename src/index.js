6ac30e6733bc868dd1b031deb40b3c8b5789fa63
tania.voyat <tetiana.voyat@gmail.com>
2021-02-01 12:15:48 +0200

    adds slider and form validation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import './style.scss';
 import toggleMenu from './js/menuToggler';
 import showAnswer from './js/showAnswer';
+import validateForm from './js/formValidation';
+import Splide from '@splidejs/splide';
+
+new Splide('#splide', {
+  classes: {
+    arrows: 'splide__arrows slider__arrows',
+  },
+}).mount();
 
 toggleMenu();
 showAnswer();
+validateForm();
